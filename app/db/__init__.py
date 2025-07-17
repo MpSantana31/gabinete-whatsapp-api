2e643d1b24207b23cf82760150e78dd50c4461fa
MpSantana31 <marcos.paulo.t08@gmail.com>
2025-07-17 13:12:22 -0300

    feat: Criação do pacote db

---
+++ app/db/__init__.py
@@ -0,0 +1,5 @@
+from app.db.models.message import ChatHistory
+from app.db.models.contact import Contact
+from app.db.models.solicitacao import Solicitacao
+
+__all__ = ['ChatHistory', 'Contact', 'Solicitacao']
